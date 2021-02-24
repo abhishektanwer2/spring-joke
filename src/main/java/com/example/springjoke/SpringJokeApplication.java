c3e9eb1f4677f006cf01501ea5a61ac85f8d2b39
Abhishek Tanwer <abhishektanwer2@gmail.com>
2021-02-24 14:43:40 -0500

    Initial commit

---
+++ src/main/java/com/example/springjoke/SpringJokeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJokeApplication.class, args);
+    }
+
+}
